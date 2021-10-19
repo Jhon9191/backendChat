ccbb45ec2b7a2233da5fcdab74d12f9506930c86
Jhon9191 <joaoantonio15161516@gmail.com>
2021-10-18 21:37:57 -0300

    Configurinh broadcast

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const cors = require('cors')
 
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Methods', 'GET', 'POST', 'PUT', 'DELETE');
     res.header('Access-Control-Allow-Headers', "X-PINGOTHER, Content-Type, Authorization")
     app.use(cors());
     next();
@@ -22,5 +22,18 @@ const server = app.listen(8080, () => {
 io = socket(server, { cors: { origin: "*" } });
 
 io.on('connect', (socket) => {
-    console.log(socket.id);
+    //console.log(socket.id);
+
+    socket.on("sala_conectada", (dados) => {
+        //console.log("Sala conectada" + dados);
+        socket.join(dados);
+    });
+
+    socket.on("enviar_mensagem", (dados) => {
+        //console.log(dados);
+        socket.to(dados.sala).emit("mensagem_dados", dados.conteudo);
+       //console.log(dados.conteudo);
+    });
+
 });
+
